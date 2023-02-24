df89c5908bae51e709a53052236444e59b3e1104
plamen22 <plamenshishmanov@gmail.com>
2023-02-24 15:07:14 +0200

    up date

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Y
+)src/test/java/resources/config.properties,d\4\d45e172f934a02016617b44ad8a207d1d3ebe70c
+P
+ src/test/java/Base/TestUtil.java,1\a\1a2ae8a6b9dd4b8c6cb84a1fb212416dcf63a187
+X
+(src/test/java/resources/correctUsers.csv,c\6\c6f4491a39decc440cd4235d8e9bfe79de6cdf87
+V
+&src/test/java/resources/wrongUsers.csv,9\4\94024d82e98c9e10b9fe4f1aae2e08e3c6bedba4
+X
+(src/test/java/page/objects/BasePage.java,f\1\f1949a4912074f84b2a68a00267a6de670f86b44
+Y
+)src/test/java/page/objects/LoginPage.java,a\a\aa37dee5aff1faaeff39d5b3fa90b713589e788a
+b
+2src/test/java/page/objects/LoginWithAddToCart.java,1\c\1c87cc741fddc3adf8395e26f495a0b55306edb7
+[
++src/test/java/page/objects/PaymentPage.java,0\8\085ba4a3c14b060550b45d75b6f569503303b70b
--- src/test/java/course/work/UnccessufulLogin.java
+++ src/test/java/course/work/UnccessufulLogin.java
@@ -5,6 +5,8 @@ import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -12,26 +14,30 @@ import page.objects.UnccessufulLoginPage;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.List;
 
 public class UnccessufulLogin extends TestUtil {
 
-    @Test (dataProvider = "wrongUsers")
-    public void UnsuccessufulLogin(String userName, String password){
+    @Test(dataProvider = "wrongUsers")
+    public void UnsuccessufulLogin(String userName, String password) {
         UnccessufulLoginPage unccessufulLoginPage = new UnccessufulLoginPage(driver);
-        unccessufulLoginPage.login(userName,password);
+        unccessufulLoginPage.login(userName, password);
 
-      WebElement errorMessage = driver.findElement (By.id("signin2"));
-        Assert.assertEquals(errorMessage.getText(),
-             "Sign up");
- }
+        WebElement signin2 = driver.findElement(By.id("signin2"));
+        WebDriverWait wait10 = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait10.until(ExpectedConditions.elementToBeClickable(signin2));
+        Assert.assertEquals(signin2.getText(),
+                "Sign up");
+    }
+
     @DataProvider(name = "wrongUsers")
     public Object[][] readUsersFromCvs() {
         try {
             CSVReader csvReader = new CSVReader(new FileReader("src/test/java/resources/wrongUsers.csv"));
             List<String[]> csvData = csvReader.readAll();
             Object[][] csvDataObj = new Object[csvData.size()][2];
-            for (int i = 0; i< csvData.size(); i ++){
+            for (int i = 0; i < csvData.size(); i++) {
                 csvDataObj[i] = csvData.get(i);
             }
             return csvDataObj;
